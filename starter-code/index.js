d0b48798cbf1f34523bd273a5a14ef82893918c5
JensErhardt <jens-erhardt@web.de>
2019-03-08 20:02:30 +0100

    Implemented constructor, add, get, max, min, sum and average functions.

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,52 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+
+  constructor() {
+    this.items = new Array();
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.length = this.items.push(item);
+    this.items.sort();
+  }
+
+  get(pos) { 
+    return this.items[pos - 1];
+  }
+
+  max() { 
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.items[this.length - 1];
+  }
+
+  min() { 
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.items[0];
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return Math.round(this.sum() / this.length);
+   }
+
+  sum() { 
+    if (this.length === 0) {
+      return 0;
+    }
+
+    return this.items.reduce(function(acc, cur) {
+      return acc + cur;
+    });
+  }
 };
 
 module.exports = SortedList;
